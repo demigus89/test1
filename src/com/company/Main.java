dcb4f6c145e9e23355d8fd303384c1ec40e40dac
demigus89 <demigus89@ukr.net>
2021-07-09 22:56:07 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Выведи число, которое будет делится на 3 без остатка");
+        /*если чичло 9,тогда метод возвращать  true;
+        если чичло 25,тогда метод возвращать  false;
+        если чичло 9,тогда метод возвращать  true;*/
+
+        myMethod (9);
+        myMethod (25);
+        myMethod (102);
+
+    }
+
+    public static void myMethod(int a) {
+        int b = 3;
+        int d = a % b;
+        if (d == 0) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+    }
+}
